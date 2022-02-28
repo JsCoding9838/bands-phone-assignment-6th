5a0037795f0ab9f90ebc1b1d04022450f45b39eb
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-03-01 04:53:04 +0800

    phone details updated

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const allPhones = () => {
     fetch(url)
     .then(res => res.json())
     .then(data => showPhones(data.data));
-    // document.getElementById('search-box').value = '';
+    document.getElementById('search-box').value = '';
 };
 
 const showPhones = phones => {
@@ -24,14 +24,10 @@ const showPhones = phones => {
                     <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h5 class="card-title">Card title</h5>
-                    <h5 class="card-title">Card title</h5>
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        This is a wider card with supporting text below as a natural lead-in to
-                    </p>
-                    <button type="button" class="btn btn-primary btn-lg mx-auto">Phone Details</button>
+                    <h5 class="fw-bold card-title">Name:<span> ${phone.phone_name}</span></h5>
+                    <h5 class="fw-bold card-title">Brand: ${phone.brand}</h5>
+                    <h5 class="fw-bold card-title">Slug: ${phone.slug}</h5>
+                    <button type="button" onclick="phoneDetails('${phone.slug}')" class="btn btn-success btn-sm mb-2">See Details</button>
                 </div>
             </div>
         `;
@@ -40,3 +36,33 @@ const showPhones = phones => {
     
 }
 
+const phoneDetails = (phoneName) => {
+    // console.log(phoneName);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneName}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showPhoneDetails(data.data))
+};
+
+const showPhoneDetails = (phoneInfo) => {
+    console.log(phoneInfo.slug);
+    console.log(phoneInfo.mainFeatures.storage);
+    console.log(phoneInfo.mainFeatures.sensors);
+
+    document.getElementById('phone-details-container').innerHTML = `
+        <div class="row">
+            <h4>Phones Details</h4>
+            <div class="col-md-6 mx-auto mb-3">
+                <img src="${phoneInfo.image}" class="phn-details-img w-50 card-img-top img-fluid" alt="phone-image">
+            </div>
+            <div class="col-md-6 text-start p-4 align-items-center mt-4">
+                <h6 class="fw-bold">Name: <span class="font-style">${phoneInfo.name}</span></h6>
+                <h6 class="fw-bold">ReleaseDate: <span class="font-style">${phoneInfo.releaseDate}</span></h6>
+                <h6 class="fw-bold">ChipSet: <span class="font-style">${phoneInfo.mainFeatures.chipSet}</span></h6>
+                <h6 class="fw-bold">Storage: <span class="font-style">${phoneInfo.mainFeatures.storage}</span></h6>
+                <h6 class="fw-bold">DisplaySize: <span class="font-style">${phoneInfo.mainFeatures.displaySize}</span></h6>
+                <h6 class="fw-bold">Memory: <span class="font-style">${phoneInfo.mainFeatures.memory}</span></h6>
+            </div>
+        </div>
+    `;
+}
