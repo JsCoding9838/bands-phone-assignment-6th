017ef81cd296b6f434772fca5ec066c222a22a2b
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-03-02 01:36:07 +0800

    error handle

--- js/app.js
+++ js/app.js
@@ -50,30 +50,29 @@ const showPhones = phones => {
     };
     // const newValue = phones.slice(0, 20);
     // const newValue2 = phones.slice(0, 30);
-
     // less than 20 phones
     // newValue2?.forEach(phone => {
-        // console.log(phone);
-
-
-    for(const phone of phones.slice(0, 20)){
+    for(const phone of phones){
         const div = document.createElement('div');
         div.classList.add('col');
 
         div.innerHTML = `
-            <div class="card h-100">
+            <div id="card" class="card h-100">
                 <div class="mx-auto">
-                    <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+                    <img src="${phone.image}" class="card-img-top img-fluid" alt="card-image">
                 </div>
                 <div class="card-body">
                     <h5 class="fw-bold card-title">Name: <span class="font-style">${phone.phone_name}</span></h5>
                     <h5 class="fw-bold card-title">Brand: <span class="font-style">${phone.brand}</span></h5>
                     <h5 class="fw-bold card-title">Slug: <span class="font-style">${phone.slug}</span></h5>
-                    <button type="button" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary btn-sm mt-2 px-4 py-1 fw-bold fs-4">See Details</button>
+                    
+
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                        <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-2 fw-bold fs-5" type="button">See Details</button>
+                    </div>
                 </div>
             </div>
         `;
-        
         phoneContainer.appendChild(div);
         
     };
@@ -90,25 +89,29 @@ const phoneDetails = (phoneName) => {
 };
 
 const showPhoneDetails = (phoneInfo) => {
-    // console.log(phoneInfo.slug);
-    // console.log(phoneInfo.mainFeatures.storage);
-    // console.log(phoneInfo.mainFeatures.sensors);
     const phnDetailsContainer = document.getElementById('phone-details-container');
     phnDetailsContainer.textContent = '';
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
     rowDiv.innerHTML = `
-        <h4>Phone Details</h4>
-        <div class="col-md-6 mx-auto mb-3">
-            <img src="${phoneInfo.image}" class="details-img w-50 card-img-top img-fluid" alt="phone-image">
+        <h4 id="details-img-title">Phone Details</h4>
+        <div class="col-md-6 mx-auto">
+            <img src="${phoneInfo.image}" class="details-img img-fluid" alt="phone-image">
         </div>
-        <div class="col-md-6 text-start p-4 align-items-center mt-4">
-            <h6 class="fw-bold">Name: <span class="font-style">${phoneInfo.name}</span></h6>
-            <h6 class="fw-bold">ReleaseDate: <span class="font-style">${phoneInfo.releaseDate ? phoneInfo.releaseDate : 'No ReleaseDate found'}</span></h6>
-            <h6 class="fw-bold">ChipSet: <span class="font-style">${phoneInfo.mainFeatures.chipSet}</span></h6>
-            <h6 class="fw-bold">Storage: <span class="font-style">${phoneInfo.mainFeatures.storage}</span></h6>
-            <h6 class="fw-bold">DisplaySize: <span class="font-style">${phoneInfo.mainFeatures.displaySize}</span></h6>
-            <h6 class="fw-bold">Memory: <span class="font-style">${phoneInfo.mainFeatures.memory}</span></h6>
+        <div id="phone-details-container" class="col-md-6 text-start p-4 align-items-center mt-4 text-wrap">
+            <h6 class="fw-bold">Name: <span class="font-style">${phoneInfo?.name}</span></h6>
+            <h6 class="fw-bold">ReleaseDate: <span class="font-style">${phoneInfo?.releaseDate ? phoneInfo?.releaseDate : 'No ReleaseDate found'}</span></h6>
+            <h6 class="fw-bold">ChipSet: <span class="font-style">${phoneInfo?.mainFeatures?.chipSet ? phoneInfo?.mainFeatures?.chipSet : 'No ChipSet'}</span></h6>
+            <h6 class="fw-bold">Storage: <span class="font-style">${phoneInfo?.mainFeatures?.storage}</span></h6>
+            <h6 class="fw-bold">DisplaySize: <span class="font-style">${phoneInfo?.mainFeatures?.displaySize}</span></h6>
+            <h6 class="fw-bold">Memory: <span class="font-style">${phoneInfo?.mainFeatures?.memory}</span></h6>
+            <h6 class="fw-bold">Sensor: <span class="font-style">${phoneInfo?.mainFeatures?.sensors}</span></h6>
+            <h6 class="fw-bold">WLAN: <span class="font-style">${phoneInfo?.others?.WLAN ? phoneInfo?.others?.WLAN : 'NO WLAN'}</span></h6>
+            <h6 class="fw-bold">Bluetooth: <span class="font-style">${phoneInfo?.others?.Bluetooth ? phoneInfo?.others?.Bluetooth : 'No Bluetooth'}</span></h6>
+            <h6 class="fw-bold">GPS: <span class="font-style">${phoneInfo?.others?.GPS ? phoneInfo?.others?.GPS : 'No GPS'}</span></h6>
+            <h6 class="fw-bold">NFC: <span class="font-style">${phoneInfo?.others?.NFC ? phoneInfo?.others?.NFC : 'No NFC'}</span></h6>
+            <h6 class="fw-bold">Radio: <span class="font-style">${phoneInfo?.others?.Radio ? phoneInfo?.others?.Radio : 'No Radio'}</span></h6>
+            <h6 class="fw-bold">USB: <span class="font-style">${phoneInfo?.others?.USB ? phoneInfo?.others?.USB : 'No USB'}</span></h6>
         </div>
     `;
     phnDetailsContainer.appendChild(rowDiv);
