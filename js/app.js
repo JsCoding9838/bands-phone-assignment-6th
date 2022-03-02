adfc0862204f86a477a9d23a39a319a52d52195f
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-03-03 00:25:05 +0800

    see more buton updated

--- js/app.js
+++ js/app.js
@@ -55,10 +55,9 @@ const showPhones = phones => {
                         <h5 class="fw-bold card-title">Name: <span class="font-style">${phone.phone_name}</span></h5>
                         <h5 class="fw-bold card-title">Brand: <span class="font-style">${phone.brand}</span></h5>
                         <h5 class="fw-bold card-title">Slug: <span class="font-style">${phone.slug}</span></h5>
-                        
     
                         <div class="d-grid gap-2 col-6 mx-auto">
-                            <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-2 fw-bold fs-5" type="button">See Details</button>
+                            <button id="card-btn" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-2 fw-bold fs-5" type="button">See Details</button>
                         </div>
                     </div>
                 </div>
@@ -84,8 +83,6 @@ const showPhones = phones => {
                         <h5 class="fw-bold card-title">Name: <span class="font-style">${phone.phone_name}</span></h5>
                         <h5 class="fw-bold card-title">Brand: <span class="font-style">${phone.brand}</span></h5>
                         <h5 class="fw-bold card-title">Slug: <span class="font-style">${phone.slug}</span></h5>
-                        
-    
                         <div class="d-grid gap-2 col-6 mx-auto">
                             <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-2 fw-bold fs-5" type="button">See Details</button>
                         </div>
@@ -112,7 +109,6 @@ const showPhones = phones => {
                         <h5 class="fw-bold card-title">Name: <span class="font-style">${phone.phone_name}</span></h5>
                         <h5 class="fw-bold card-title">Brand: <span class="font-style">${phone.brand}</span></h5>
                         <h5 class="fw-bold card-title">Slug: <span class="font-style">${phone.slug}</span></h5>
-                        
     
                         <div class="d-grid gap-2 col-6 mx-auto">
                             <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary mt-2 fw-bold fs-5" type="button">See Details</button>
@@ -144,11 +140,12 @@ const showPhoneDetails = (phoneInfo) => {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
     rowDiv.innerHTML = `
-        <h4 id="details-img-title">Phone Details</h4>
+        <h4 id="details-img-title">Phone Details 2022</h4>
         <div class="col-md-6 mx-auto">
             <img src="${phoneInfo.image}" class="details-img img-fluid" alt="phone-image">
         </div>
         <div id="phone-details-container" class="col-md-6 text-start p-4 align-items-center mt-4 text-wrap">
+
             <h6 class="fw-bold">Name: <span class="font-style">${phoneInfo?.name}</span></h6>
             <h6 class="fw-bold">ReleaseDate: <span class="font-style">${phoneInfo?.releaseDate ? phoneInfo?.releaseDate : 'No ReleaseDate found'}</span></h6>
             <h6 class="fw-bold">ChipSet: <span class="font-style">${phoneInfo?.mainFeatures?.chipSet ? phoneInfo?.mainFeatures?.chipSet : 'No ChipSet'}</span></h6>
@@ -162,6 +159,7 @@ const showPhoneDetails = (phoneInfo) => {
             <h6 class="fw-bold">NFC: <span class="font-style">${phoneInfo?.others?.NFC ? phoneInfo?.others?.NFC : 'No NFC'}</span></h6>
             <h6 class="fw-bold">Radio: <span class="font-style">${phoneInfo?.others?.Radio ? phoneInfo?.others?.Radio : 'No Radio'}</span></h6>
             <h6 class="fw-bold">USB: <span class="font-style">${phoneInfo?.others?.USB ? phoneInfo?.others?.USB : 'No USB'}</span></h6>
+
         </div>
     `;
     phnDetailsContainer.appendChild(rowDiv);
